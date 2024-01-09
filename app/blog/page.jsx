159d3ebd4c711c9c857d71207efb920cf72010cc
soyangelmtz <angeld.mtzg@gmail.com>
2024-01-09 14:31:17 -0600

    Bug fix to DarkMode.js

--- app/blog/page.jsx
+++ app/blog/page.jsx
@@ -1,9 +1,78 @@
-import React from 'react'
+import React from "react";
 
 function BlogPage() {
   return (
-    <div className='text-white dark:bg-gray-900'>BlogPage</div>
-  )
+<section class="py-10 bg-w sm:py-16 lg:py-24 z-40 relative">
+
+
+    <div class="container mx-auto">
+        <h2 class="text-3xl font-light   text-black sm:text-4xl lg:text-5xl">
+            it's <span class="block w-full font-light text-transparent bg-clip-text bg-gradient-to-r from-green-500 to-green-500 lg:inline">Services
+            </span> in one single line.
+        </h2>
+        <p class="mb-20 text-lg text-gray-900">Comes directly from the desk of engineers, creators and managers at
+            Skcript.
+        </p>
+
+        <div class="grid grid-cols-1 gap-6 mx-9  lg:grid-cols-4 ">
+            <a href="#" class=" shadow-2xl relative ">
+                <div class="  h-full relative shadow-2xl shadow-green-900 overflow-hidden group ">
+                    <div class=" absolute -bottom-10 group-hover:top-0 left-0 w-full h-full group-hover:bg-gray-900 transition-all ease-in-out duration-500  ">
+                        <div class="w-full h-full   p-5   relative">
+                            <div class="absolute bottom-0 group-hover:bottom-24 text-white  text-left   transition-all ease-in-out duration-500 ">
+                                <h2 class="text-2xl font-bold  text-white mb-0 pb-1">Standard Color</h2>
+                                <p class="text-lg font-light text-white">Lorem ipsum dolor sit amet, #brands.</p>
+                            </div>
+                        </div>
+                    </div>
+                    <img src="https://source.unsplash.com/random/400x400" class="w-full z-0  h-full    object-fill example " />
+                </div>
+            </a>
+            <a href="#" class=" shadow-2xl relative ">
+                <div class="  h-full relative shadow-2xl shadow-green-900 overflow-hidden group ">
+                    <div class=" absolute -bottom-10 group-hover:top-0 left-0 w-full h-full group-hover:bg-green-900 transition-all ease-in-out duration-500  ">
+                        <div class="w-full h-full   p-5   relative">
+                            <div class="absolute bottom-0 group-hover:bottom-24 text-white  text-left   transition-all ease-in-out duration-500 ">
+                                <h2 class="text-2xl font-bold  text-white mb-0 pb-1">Standard Color</h2>
+                                <p class="text-lg font-light text-white">Lorem ipsum dolor sit amet, #brands.</p>
+                            </div>
+                        </div>
+                    </div>
+                    <img src="https://source.unsplash.com/random/400x400" class="w-full z-0  h-full    object-fill example " />
+                </div>
+            </a>
+            <a href="#" class=" shadow-2xl relative ">
+                <div class="  h-full relative shadow-2xl shadow-green-900 overflow-hidden group ">
+                    <div class=" absolute -bottom-10 group-hover:top-0 left-0 w-full h-full group-hover:bg-green-900 transition-all ease-in-out duration-500  ">
+                        <div class="w-full h-full   p-5   relative">
+                            <div class="absolute bottom-0 group-hover:bottom-24 text-white  text-left   transition-all ease-in-out duration-500 ">
+                                <h2 class="text-2xl font-bold  text-white mb-0 pb-1">Standard Color</h2>
+                                <p class="text-lg font-light text-white">Lorem ipsum dolor sit amet, #brands.</p>
+                            </div>
+                        </div>
+                    </div>
+                    <img src="https://source.unsplash.com/random/400x400" class="w-full z-0  h-full    object-fill example " />
+                </div>
+            </a> 
+            <a href="#" class=" shadow-2xl relative ">
+                <div class="  h-full relative shadow-2xl shadow-green-900 overflow-hidden group ">
+                    <div class=" absolute -bottom-10 group-hover:top-0 left-0 w-full h-full group-hover:bg-green-900 transition-all ease-in-out duration-500  ">
+                        <div class="w-full h-full   p-5   relative">
+                            <div class="absolute bottom-0 group-hover:bottom-24 text-white  text-left   transition-all ease-in-out duration-500 ">
+                                <h2 class="text-2xl font-bold  text-white mb-0 pb-1">Standard Color</h2>
+                                <p class="text-lg font-light text-white">Lorem ipsum dolor sit amet, #brands.</p>
+                            </div>
+                        </div>
+                    </div>
+                    <img src="https://source.unsplash.com/random/400x400" class="w-full z-0  h-full    object-fill example " />
+                </div>
+            </a>
+
+
+        </div>
+
+</div></section>
+  );
 }
 
-export default BlogPage
+export default BlogPage;
